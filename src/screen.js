f312517a2668a09f35f4a645faa35fbf20b524dc
Joshua Koo <zz85nus@gmail.com>
2018-11-28 07:45:15 -0800

    Implement Mousemove

--- src/screen.js
+++ src/screen.js
@@ -1,25 +1,41 @@
 var $ = require('nodobjc')
 
-
-// 
-
 $.framework('Foundation') // foundation for strings/arrays
 $.framework('Appkit') // nscreens
 $.framework('CoreFoundation')
 $.framework('CoreGraphics')
 // Cocoa, IOKit
-// coregraphics?
 
 screens = $.NSScreen('screens')
+screens_count = screens('count')
+console.log(screens_count)
+
+for (let i = 0; i < screens_count; i++) {
+    const aScreen = screens('objectAtIndex', i)
+
+    console.log('Screen ' + i)
+    const backingScaleFactor = aScreen('backingScaleFactor')
+
+    const frame = aScreen('frame')
+    const description = aScreen('deviceDescription');
+    
+    // const visibleFrame = aScreen('visibleFrame')
+    // console.log(aScreen.methods())
+    console.log()
+    console.log(description)
+    // console.log('frame', frame)
+    // console.log(description('class'), description.methods())
+    console.log(description('objectForKey', $('NSDeviceSize')))
+    e = description('keyEnumerator')
+    while (key = e('nextObject')) {
+        console.log(key, description('objectForKey', key))
+    }
+
+    // NSDeviceResolution, NSDeviceSize, NSScreenNumber
+    console.log('scaling', backingScaleFactor)
+}
 
 // console.log('screens', screens);
-console.log(screens('count'), screens.methods())
-screen0 = screens('objectAtIndex', 0)
-
-console.log(screen0.methods(), screen0('frame'), screen0('userSpaceScaleFactor'))
-console.log(screen0('deviceDescription'))
-console.log(screen0('visibleFrame'))
-console.log(screen0('backingScaleFactor'))
 
 /*
 // https://github.com/Hammerspoon/hammerspoon/blob/master/extensions/screen/internal.m
@@ -42,24 +58,34 @@ console.log(screen0('backingScaleFactor'))
 
 // https://github.com/BlueM/cliclick/tree/master/Actions
 
-/*
-
- CGEventRef ourEvent = CGEventCreate(NULL);
-    CGEventGetLocation(ourEvent);
-    CFRelease(ourEvent);
-*/
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 function getMouse() {
     const ref = $.CGEventCreate(null)
     const pos = $.CGEventGetLocation(ref)
     $.CFRelease(ref)
 
-    return {
-        x: pos.x,
-        y: pos.y
-    }
+    return new Point(pos.x, pos.y);
 }
 
-setInterval(() => console.log(getMouse()), 0);
+function moveMouse(x, y) {
+    const moveEventRef = // CGEventRef
+        $.CGEventCreateMouseEvent(null,
+            $.kCGEventMouseMoved,
+            $.CGPointMake(x, y),
+            $.kCGMouseButtonLeft); // kCGMouseButtonLeft is ignored
+    $.CGEventPost($.kCGHIDEventTap, moveEventRef);
+    $.CFRelease(moveEventRef);
+}
+
+setInterval(() => {
+    moveMouse(Math.random() * 1000, Math.random() * 1000)
+    console.log(getMouse())
+}, 500);
 
 
