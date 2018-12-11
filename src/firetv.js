6c448c3c635f9318a39eb2b895c8637aecabcf15
Joshua Koo <zz85nus@gmail.com>
2018-12-11 01:02:11 -0800

    It works!

--- src/firetv.js
+++ src/firetv.js
@@ -18,13 +18,15 @@ client.connect('192.168.0.14')
     .then(ok, err)
     .then(v => client.listDevices())
     .then(devices => devices.forEach(device => {
-        return client.shell(device.id, 'echo $RANDOM')
-        // Use the readAll() utility to read all the content without
-        // having to deal with the events. `output` will be a Buffer
-        // containing all the output.
-        .then(adb.util.readAll)
-        .then(function(output) {
-          console.log('[%s] %s', device.id, output.toString().trim())
-        })
+        console.log('device', device);
+        return client.shell(device.id, 'input keyevent 20')
+            // Use the readAll() utility to read all the content without
+            // having to deal with the events. `output` will be a Buffer
+            // containing all the output.
+            .then(adb.util.readAll, err)
+            .then(function(output) {
+                console.log('out')
+                console.log('[%s] %s', device.id, output.toString().trim())
+            },)
     }), err)
 
